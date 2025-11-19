e063cfa5a9ea0a5873dfc68da056747e30ba4113
Sridevi <parimisridevi586@gmail.com>
2025-11-19 19:01:20 +1100

    added iam role for remote exec connection provisioner

--- modules/ec2/locals.tf
+++ modules/ec2/locals.tf
@@ -2,5 +2,5 @@ locals {
     tagName         = var.env == "null" ? var.name : "${var.name}-${var.env}"
     dnsPrivateName  = var.env == "null" ? "${var.name}-internal" :"${var.name}-internal"
     dnsPublicName   = var.env == "null" ? var.name : "${var.name}-${var.env}"
-    iam_policy      = concat(["sts:GetCallerIdentity","sts:AssumeRole",], var.iam_policy)
+    iam_policy      = concat(["sts:GetCallerIdentity",], var.iam_policy)
 }
