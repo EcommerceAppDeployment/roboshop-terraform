910b853ad7a8461c68d1fd76db5bbb1c063b48a4
Sridevi <parimisridevi586@gmail.com>
2025-11-19 17:44:32 +1100

    added a different local name

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,13 +3,13 @@ resource "aws_instance" "my_ec2_instance" {
     instance_type = var.instance_type
                           
     tags = {
-      Name = "${var.name}-${var.env}"
+      Name = local.tagName
     }
 }
 
 resource "aws_route53_record" "public_record" {
   zone_id = var.zone_id
-  name    = "${var.name}-${var.env}"
+  name    = local.dnsPublicName
   type    = "A"
   ttl     = 300
   records = [aws_instance.my_ec2_instance.public_ip]
@@ -17,7 +17,7 @@ resource "aws_route53_record" "public_record" {
 
 resource "aws_route53_record" "private_record" {
   zone_id = var.zone_id
-  name    = "${var.name}-${var.env}"
+  name    = local.dnsPrivateName
   type    = "A"
   ttl     = 300
   records = [aws_instance.my_ec2_instance.private_ip]
