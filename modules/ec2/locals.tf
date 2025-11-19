9d9b90a867c7deb20394f4ed1b7a6d2d01bd6806
Sridevi <parimisridevi586@gmail.com>
2025-11-19 17:56:02 +1100

    added a different local name

--- modules/ec2/locals.tf
+++ modules/ec2/locals.tf
@@ -1,5 +1,5 @@
 locals {
     tagName         = var.env == "null" ? var.name : "${var.name}-${var.env}"
-    dnsPrivateName  = var.env == "null" ? var.name :"${var.name}-internal"
+    dnsPrivateName  = var.env == "null" ? "${var.name}-internal" :"${var.name}-internal"
     dnsPublicName   = var.env == "null" ? var.name : "${var.name}-${var.env}"
 }
