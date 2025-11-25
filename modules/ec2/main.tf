c95fd169c8497c4458037869cf38e960bf17eba0
Sridevi <parimisridevi586@gmail.com>
2025-11-25 19:47:42 +1100

    added log recording

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -45,7 +45,7 @@ resource "null_resource" "ansible" {
       host        = aws_instance.my_ec2_instance.private_ip
     }
     inline = [
-      "ansible-pull -i localhost, -U https://github.com/EcommerceAppDeployment/roboshop-ansible playbook.yml -e role=${var.name} -e env=${var.env} -e token=${var.token}"
+      "ansible-pull -i localhost, -U https://github.com/EcommerceAppDeployment/roboshop-ansible playbook.yml -e role=${var.name} -e env=${var.env} -e token=${var.token} | sudo tee /opt/ansible.log"
     ]
   }
 }
